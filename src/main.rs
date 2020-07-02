94f0c4dcb86d3ff885d5ab8ce55765ee5731a5c4
royalmustard <36129876+royalmustard@users.noreply.github.com>
2020-07-02 10:36:42 +0200

    scale optional and more user friendly

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,32 @@ mod gyptlib;
 
 fn main() {
     env::args().for_each(|a| println!("{:?}", a));
+    if env::args().count() < 4
+    {
+        println!("Number of arguments entered too small! Aborting");
+        print_usage();
+        return
+    }
     let obj_filename = env::args().nth(1).unwrap();
     let output_filename = env::args().nth(2).unwrap();
     let particle_radius = env::args().nth(3).unwrap().parse::<f32>().unwrap();
-    let scale = env::args().nth(4).unwrap().parse::<f32>().unwrap();
+    let scale_opt = env::args().nth(4).unwrap().parse::<f32>();
+    let mut scale = Vector3::new(1.0, 1.0, 1.0);
+    
+    if let Ok(u_scale) = scale_opt
+    {
+        scale = Vector3::new(u_scale, u_scale, u_scale);
+    }
 
-    let mesh = gyptlib::trimesh_from_obj(&obj_filename).scaled(&Vector3::new(scale, scale, scale));
+    let mesh = gyptlib::trimesh_from_obj(&obj_filename).scaled(&scale);
     
     
     let sample = salva3d::sampling::shape_volume_ray_sample(&mesh, particle_radius).unwrap();
 
     gyptlib::save_salva_sample(&output_filename, sample);
+}
+
+fn print_usage()
+{
+    println!("Usage: salva_sampler <Path to obj file> <Path to output file> <particle radius> Option<scale>");
 }
